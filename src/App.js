b7e1e66e02c9a5435412554defec176436504773
wanglei <18510931963@163.com>
2019-09-19 22:11:52 +0800

    练习2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Calculator from './views/state1/calculator'
+// import Calculator from './components/state1/calculator'
+// import WelcomeDialog from './components/extends/extends'
+import Lrapp from './components/extend2/extend2'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Calculator/>
+        {/* <Calculator/> */}
+        {/* <WelcomeDialog /> */}
+        <Lrapp/>
         <a
           className="App-link"
           href="https://reactjs.org"
